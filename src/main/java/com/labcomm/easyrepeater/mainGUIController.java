9001805a94f66e6a5c511c2047599ddc7c5a3136
Marc Serret <marc.serret@duagon.com>
2023-02-07 11:48:37 +0100

    Refactor

--- src/main/java/com/labcomm/easyrepeater/mainGUIController.java
+++ src/main/java/com/labcomm/easyrepeater/mainGUIController.java
@@ -4,9 +4,14 @@ import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.scene.control.*;
+import javafx.scene.control.Alert.AlertType;
+
 import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
+
+import com.labcomm.easyrepeater.constants.ApplicationConstants;
+import com.labcomm.easyrepeater.constants.ApplicationLiterals;
 
 public class mainGUIController {
     @FXML
@@ -15,67 +20,82 @@ public class mainGUIController {
     private TextField portlabel;
     @FXML
     private ChoiceBox cbox;
-    ObservableList<String> connectionType = FXCollections.observableArrayList("Server","Client");
+    ObservableList<String> connectionType = FXCollections.observableArrayList(ApplicationConstants.SERVER, ApplicationConstants.CLIENT);
     @FXML
     private TextArea trama;
 
-    public Socket socket;
+    private Socket socket;
+
     public void setSocket(Socket socket) {
         this.socket = socket;
     }
+
     public Socket getSocket() {
         return socket;
     }
+
     private void initSocket() {
         String ip = iplabel.getText();
         String port = portlabel.getText();
         String type = cbox.getValue().toString();
-            if (type.equals("Server")) {
-                new Thread(() -> {
-                    try {
-                        ServerSocket serverSocket = new ServerSocket(Integer.parseInt(port));
-                        setSocket(serverSocket.accept());
-                    } catch (IOException e) {
-                        Alert alert = new Alert(Alert.AlertType.ERROR);
-                        alert.setTitle("Error");
-                        alert.setHeaderText("Connection failed");
-                        alert.setContentText("Failed to start server: " + e.getMessage());
-                        alert.showAndWait();
-                    }
-                }).start();
-                Alert alert = new Alert(Alert.AlertType.INFORMATION);
-                alert.setTitle("Success");
-                alert.setHeaderText("Connection established");
-                alert.setContentText("The connection file has been created successfully.");
-                alert.showAndWait();
-                System.out.println(socket);
-            }
-            if (type.equals("Client")) {
-                try {
-                    setSocket(new Socket(ip, Integer.parseInt(port)));
-                    Alert alert = new Alert(Alert.AlertType.INFORMATION);
-                    alert.setTitle("Success");
-                    alert.setHeaderText("Connection established");
-                    alert.setContentText("The connection file has been created successfully.");
-                    alert.showAndWait();
-                    System.out.println(socket);
-                } catch (IOException e) {
-                    Alert alert = new Alert(Alert.AlertType.ERROR);
-                    alert.setTitle("Error");
-                    alert.setHeaderText("Connection failed");
-                    alert.setContentText("Failed to connect to the server: " + e.getMessage());
-                    alert.showAndWait();
-                }
-            }
+        switch (type) {
+            case ApplicationConstants.SERVER:
+                socketServer(ip, port, type);
+                break;
+            case ApplicationConstants.CLIENT:
+                socketClient(ip, port, type);
+                break;
+            default:
+                System.out.println(ApplicationLiterals.MASSIVE_ERROR);
+                break;
         }
+    }
+
+    private void socketServer(String ip, String port, String type) {
+        new Thread(() -> {
+            try {
+                ServerSocket serverSocket = new ServerSocket(Integer.parseInt(port));
+                setSocket(serverSocket.accept());
+            } catch (IOException e) {
+                createAlert(ApplicationLiterals.ERROR, ApplicationLiterals.CONNECTION_FAILED,
+                        ApplicationLiterals.FAILED_START_SERVER, AlertType.ERROR);
+            }
+        }).start();
+        createAlert(ApplicationLiterals.SUCCESS, ApplicationLiterals.CONNECTION_ESTABLISHED,
+                ApplicationLiterals.CONNECTION_FILE_CREATED, AlertType.INFORMATION);
+        System.out.println(socket);
+    }
+
+    private void socketClient(String ip, String port, String type) {
+        try {
+            setSocket(new Socket(ip, Integer.parseInt(port)));
+            createAlert(ApplicationLiterals.SUCCESS, ApplicationLiterals.CONNECTION_ESTABLISHED,
+                    ApplicationLiterals.CONNECTION_FILE_CREATED, AlertType.INFORMATION);
+            System.out.println(socket);
+        } catch (IOException e) {
+            createAlert(ApplicationLiterals.ERROR, ApplicationLiterals.CONNECTION_FAILED,
+                    ApplicationLiterals.FAILED_TO_CONNECT.concat(e.getMessage()), AlertType.ERROR);
+        }
+    }
+
+    private void createAlert(String title, String header, String content, AlertType alertType) {
+        Alert alert = new Alert(alertType);
+        alert.setTitle(title);
+        alert.setHeaderText(header);
+        alert.setContentText(content);
+        alert.showAndWait();
+    }
+
     @FXML
-    private void choiceBoxType(){
+    private void choiceBoxType() {
         cbox.setItems(connectionType);
     }
+
     @FXML
     protected void handleSetConnectionAction() {
-       initSocket();
+        initSocket();
     }
+
     @FXML
     private void handleSendTraceAction() {
         String tramaText = trama.getText();
@@ -86,18 +106,12 @@ public class mainGUIController {
                 OutputStream outputStream = socket.getOutputStream();
                 outputStream.write(tramaText.getBytes());
             } catch (IOException e) {
-                Alert alert = new Alert(Alert.AlertType.ERROR);
-                alert.setTitle("Error");
-                alert.setHeaderText("Failed to send trace");
-                alert.setContentText("Failed to send trace: " + e.getMessage());
-                alert.showAndWait();
+                createAlert(ApplicationLiterals.ERROR, ApplicationLiterals.FAILED_SEND_TRACE,
+                        ApplicationLiterals.FAILED_SEND_TRACE.concat(e.getMessage()), AlertType.ERROR);
             }
         } else {
-            Alert alert = new Alert(Alert.AlertType.ERROR);
-            alert.setTitle("Error");
-            alert.setHeaderText("No connection established");
-            alert.setContentText("Please establish a connection before sending the trace.");
-            alert.showAndWait();
+            createAlert(ApplicationLiterals.ERROR, ApplicationLiterals.NO_CONNECTION_ESTABLISHED,
+                    ApplicationLiterals.ESTABLISH_CONNECTION, AlertType.ERROR);
         }
     }
 }
